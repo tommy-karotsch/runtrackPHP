b340828defd9abe3195bf045da77d8cf76a45d88
Tommy <tommy.karotsch@laplateforme.io>
2026-01-07 11:49:52 +0100

    runtrackphp_jour_2

--- jour2/job05/index.php
+++ jour2/job05/index.php
@@ -1,6 +1,27 @@
 <?php
 
-for ($i = 0; $i < 1001; $i ++){
-
-    echo $i."<br>";
+function primeNumber($num){
+    if ($num <= 1){
+        return false;
+    }
+    
+    for ($i = 2; $i <= sqrt($num); $i++){
+        if ($num % $i == 0){
+            return false;
+        }
+    }
+    
+    return true;
 }
+
+echo "Nombres premiers jusqu'à 1000 :<br><br>";
+
+$count = 0;
+for ($number = 2; $number <= 1000; $number++){
+    if (primeNumber($number)){
+        echo $number . "<br><br>";
+        $count++;
+    }
+}
+
+echo "<br>Total : $count nombres premiers";
